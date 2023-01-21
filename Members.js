1bf7bf80fbc3e840ba97af28bfda8738f412ade9
Chepkonga-elli <kiptoochelimo219@gmail.com>
2023-01-21 08:27:44 +0300

    Added several files of express-members-app

---
+++ Members.js
@@ -0,0 +1,30 @@
+
+const members = [
+    {
+        id:1,
+        name:'John Doe',
+        email:'john@gmail.com',
+        status:'active'
+    },
+    {
+        id:2,
+        name:'Jane Eyre',
+        email:'jane@yahoo.com',
+        status:'active'
+    },
+    {
+        id:3,
+        name:'Shannon Meyer',
+        email:'shannon@gmail.com',
+        status:'inactive'
+    },
+    {
+        id:4,
+        name:'Felix Kiptoo',
+        email:'felix@hotmail.com',
+        status:'active'
+    }
+    
+]
+
+module.exports = members
